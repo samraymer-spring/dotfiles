98337b2e71c0163ceb437ef0d5608b7b3a84d24b
Sam Raymer <sam@sam-raymer.com>
2026-07-30 00:33:16 -0700

    zsh prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -7,7 +7,19 @@ setopt prompt_subst
 # Get vcs info before showing prompt
 add-zsh-hook precmd vcs_info
 
-# zstyle ':vcs_info:*' formats ' %s(%F{red}%b%f)' # git(main)
-zstyle ':vcs_info:*' formats ' %F{2}%s%F{7}:%F{2}(%F{1}%b%F{2})%f'
+zstyle ':vcs_info:*' check-for-changes true
 
-PROMPT='%n@%m %1~%F{red}${vcs_info_msg_0_}%f λ '
+# asterisk in unstaged state
+zstyle ':vcs_info:*' unstagedstr ' *'
+# plus in staged state
+zstyle ':vcs_info:*' stagedstr ' +'
+
+# zstyle ':vcs_info:*' formats ' %s(%F{red}%b%f)' # git(main)
+# catchall vcs info
+zstyle ':vcs_info:*' formats ' %F{2}%s%F{7}:%F{2}(%F{1}%b%u%c%F{2})%f'
+# git specific vcs info
+zstyle ':vcs_info:git:*' formats ' %F{2}(%F{1}%b%u%c%F{2})%f'
+# vcs info during git action
+zstyle ':vcs_info:git:*' actionformats ' %F{2}(%F{1}%b|%a%u%c%F{2})%f'
+
+PROMPT='%n@%m %1~${vcs_info_msg_0_} λ '
